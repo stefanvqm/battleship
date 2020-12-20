08b0e1c2aed7211c0d0a35c31cd8524aa979e912
stefanPC <stefab@DESKTOP-P8D0PS2>
2020-12-20 15:32:37 +0100

    branch testing finished

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ const char buchstaben[] = {'a','b','c','d','e','f','g','h','i','j'};
 char boatfield1[10][10];    // [y][x]  field for player 1
 char boatfield2[10][10];    // [y][x]  field for player 2
 
-// testing branch
+
 
 void deleteall() { 
     // deletes the terminal for changing user. //  # präprozessor anweisungen     
